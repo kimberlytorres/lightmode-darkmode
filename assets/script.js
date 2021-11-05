7f74f823b3a44d9f75235500a3f8afd595e55d58
kimberlytorres <kimberly.torres.mattos@gmail.com>
2021-11-05 19:13:14 -0300

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,35 @@
+function changeMode () {
+    changeClasses();
+    changeText();
+    changeImg();
+}
+
+function changeClasses() {
+    button.classList.toggle(darkModeClass);
+    h1.classList.toggle(darkModeClass);
+    description.classList.toggle(darkModeClass);
+    body.classList.toggle(darkModeClass);
+    footer.classList.toggle(darkModeClass);
+}
+
+function changeText() {
+    if(body.classList.contains(darkModeClass)) {
+        button.innerHTML = '<i class="far fa-sun"></i> Change theme';
+        description.innerHTML = 'You are currently viewing it in Dark Mode.'
+        
+    }
+}
+
+function changeImg() {
+    document.getElementById('image').src="imgs/hugo-moon.png"
+
+}
+
+const darkModeClass = 'dark-mode'
+const button = document.getElementById('mode-selector');
+const h1 = document.getElementById('main-title');
+const description = document.getElementById('description');
+const body = document.getElementsByTagName('body')[0];
+const footer = document.getElementsByTagName('footer')[0];
+
+button.addEventListener('click', changeMode);
